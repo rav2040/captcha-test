396869678f2433a903355a3d6f474b664af9aaad
Stuart Kennedy <rav2040@gmail.com>
2023-07-09 19:50:39 +1200

    Add links

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,16 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Link href="/google/v3">Google reCAPTCHA v3</Link>
-        <Link href="/google/v2">Google reCAPTCHA v2</Link>
-        <Link href="/google/v2-invisible">Google reCAPTCHA v2 (Invisible)</Link>
-        <Link href="/hcaptcha">hCaptcha</Link>
-        <Link href="/cloudflare/managed">Cloudflare (Managed)</Link>
-        <Link href="/cloudflare/non-interactive">Cloudflare (Non-interactive)</Link>
-        <Link href="/cloudflare/invisible">Cloudflare (Invisible)</Link>
+      <div className="z-10 w-full max-w-5xl items-center justify-between">
+        <ul >
+          <li><Link className="text-indigo-600 hover:text-blue-800" href="/google/v3">Google reCAPTCHA v3</Link></li>
+          <li><Link className="text-indigo-600 hover:text-blue-800" href="/google/v2">Google reCAPTCHA v2</Link></li>
+          <li><Link className="text-indigo-600 hover:text-blue-800" href="/google/v2-invisible">Google reCAPTCHA v2 (Invisible)</Link></li>
+          <li><Link className="text-indigo-600 hover:text-blue-800" href="/hcaptcha">hCaptcha</Link></li>
+          <li><Link className="text-indigo-600 hover:text-blue-800" href="/cloudflare/managed">Cloudflare (Managed)</Link></li>
+          <li><Link className="text-indigo-600 hover:text-blue-800" href="/cloudflare/non-interactive">Cloudflare (Non-interactive)</Link></li>
+          <li><Link className="text-indigo-600 hover:text-blue-800" href="/cloudflare/invisible">Cloudflare (Invisible)</Link></li>
+        </ul>
       </div>
     </main>
   )
